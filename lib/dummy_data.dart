c0bb061481e897d46ceaa26b6035ca67f549b308
ilhomjon <catchy977@gmail.com>
2022-09-29 16:07:28 +0500

    Start Project

---
+++ lib/dummy_data.dart
@@ -0,0 +1,12 @@
+import './models/category.dart';
+import 'package:flutter/material.dart';
+
+const DUMMY_CATEGORIES = [
+  Category(id: 'c1', title: 'Italian', color: Colors.purple),
+  Category(id: 'c2', title: 'Quick & Easy', color: Colors.red),
+  Category(id: 'c3', title: 'Hamburgers', color: Colors.orange),
+  Category(id: 'c4', title: 'German', color: Colors.amber),
+  Category(id: 'c5', title: 'Hamburgers', color: Colors.orange),
+  Category(id: 'c6', title: 'Hamburgers', color: Colors.orange),
+  Category(id: 'c7', title: 'Hamburgers', color: Colors.orange),
+];
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:deli_meals/categories_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
